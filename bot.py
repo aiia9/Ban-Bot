4fefb3d533764a608a9ab64d7a40293bf8440e2b
aiia9 <150587154+aiia9@users.noreply.github.com>
2024-09-28 21:35:26 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,9 +15,7 @@ def ban_user(message):
         user_id = message.from_user.id
         user_status = bot.get_chat_member(chat_id, user_id).status 
             # проверка пользователя
-        if user_status == 'administrator' or user_status == 'creator':
-            bot.reply_to(message, "Невозможно забанить администратора.")
-        else:
+        if user_status != 'administrator' or user_status != 'creator':
             bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
 
